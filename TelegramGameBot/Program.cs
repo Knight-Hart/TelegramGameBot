afec6a4eb0b9122340841c15aa9fe2ba509a34a3
KnightHart <steveperson666@gmail.com>
2019-03-02 11:31:26 +0200

    Added simple story

--- TelegramGameBot/Program.cs
+++ TelegramGameBot/Program.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using Telegram.Bot;
 using Telegram.Bot.Args;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.ReplyMarkups;
 
 namespace TelegramGameBot
 {
@@ -18,6 +20,7 @@ namespace TelegramGameBot
                 botClient = new TelegramBotClient("659459848:AAGt08uNp_ubBdb8ABzrGsVC5Ia4t1DwPo4");
                 var bot = botClient.GetMeAsync().Result;
                 botClient.OnMessage += getMessage;
+                botClient.OnCallbackQuery += BotClient_OnCallbackQueryAsync;
                 botClient.StartReceiving();
             }
             catch (Exception ex)
@@ -27,12 +30,33 @@ namespace TelegramGameBot
             Console.ReadLine();
         }
 
+        private static async void BotClient_OnCallbackQueryAsync(object sender, CallbackQueryEventArgs e)
+        {
+            switch (e.CallbackQuery.Data)
+            {
+                case "/kingspeech":
+                    await botClient.SendPhotoAsync(e.CallbackQuery.Message.Chat.Id, "https://dragon-quest.org/images/thumb/7/77/KingLorikArt.png/200px-KingLorikArt.png", "Храбрый воин, наследник великого рыцаря Эрдрика, на наши земли пришло зло в виде ужасного Драгонлорда! Отправляйся в его замок, который находится в лесу на севере и уничтож Драгонлорда!");
+                    break;
+            }
+        }
+
+        private static async Task getGamePage(Chat userChat)
+        {
+            InlineKeyboardButton b = new InlineKeyboardButton();
+            b.Text = "Начать";
+            b.CallbackData = "/kingspeech";
+
+            var keys = new InlineKeyboardMarkup(b);
+            await botClient.SendTextMessageAsync(userChat.Id, "-", replyMarkup: keys);
+        }
+
         private static async void getMessage(object sender, MessageEventArgs e)
         {
             switch (e.Message.Text)
             {
                 case "/begin":
                     await botClient.SendPhotoAsync(e.Message.Chat.Id, "https://vignette.wikia.nocookie.net/fictionalcrossover/images/8/80/Dragon_Quest_logo.png/");
+                    await getGamePage(e.Message.Chat);
                     break;
                 default:
                     break;
